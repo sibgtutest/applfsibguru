e28fb529b4635ddbfa1960043bcd23e16579f17c
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-09 14:57:05 +0700

    init

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,9 +20,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        '/css/site.css',
     ];
     public $js = [
+        '/js/search.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
